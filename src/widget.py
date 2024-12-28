d69d4ec790a329194b18ab5d8c1959856b14758f
Arina <rudenskaaarina@gmail.com>
2024-12-29 00:49:09 +0500

    finished homework #2

--- src/widget.py
+++ src/widget.py
@@ -7,13 +7,16 @@ def mask_account_card(card_and_number: Optional[str]) -> str:
     """
     Возвращает строку с замаскированным номером карты или счета. Для карт и счетов используется свой тип маскировки.
     """
+
     name: Iterable[str] = []
     number: str = ""
+
     while card_and_number.split(" ")[0] == "Счет":
         for one in card_and_number.split(" "):
             if one.isdigit():
                 number = one
         return "Счет " + get_mask_account(number)
+
     for one in card_and_number.split(" "):
         if one.isalpha():
             name.append(one)
@@ -22,5 +25,11 @@ def mask_account_card(card_and_number: Optional[str]) -> str:
     return " ".join(name) + " " + get_mask_card_number(number)
 
 
-def get_date(date: Optional[int]) -> str:
-    pass
+def get_date(date: Optional[str]) -> str:
+    """
+    Принимает дату, возвращает строку в формате "ДД.ММ.ГГГГ"
+    """
+
+    preformat_date: Iterable[str] = date.split("T")[0]
+    format_date: Iterable[str] = preformat_date.split("-")[::-1]
+    return ".".join(format_date)
