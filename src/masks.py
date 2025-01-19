a8c70916195df9ba9c5ad12551be7b44c4851b00
Arina <rudenskaaarina@gmail.com>
2025-01-19 19:01:25 +0500

    masks.py tested and passed

--- src/masks.py
+++ src/masks.py
@@ -3,11 +3,17 @@ def get_mask_card_number(number: str) -> str:
        Функция принимает на вход номер карты и возвращает ее маску в формате XXXX XX** **** XXXX, где X
     — это цифра номера.
     """
-    return number[0:4] + " " + number[5:7] + "** **** " + number[-4:]
+    card_number = number.replace(" ", "")
+    if len(card_number) >= 10:
+        return card_number[0:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
+    return ""
 
 
 def get_mask_account(number: str) -> str:
     """
     Функция принимает на вход номер счета и возвращает его маску в формате **XXXX, где X — это цифра номера.
     """
-    return "**" + number[-4:]
+    card_number = number.replace(" ", "")
+    if len(card_number) >= 10:
+        return "**" + number[-4:]
+    return ""
---
+++ tests/test_masks.py
@@ -0,0 +1,23 @@
+import pytest
+from src.masks import get_mask_card_number, get_mask_account
+
+
+@pytest.mark.parametrize("input_card, expected_output", [
+    ("6757342342234326", "6757 34** **** 4326"),
+    ("5632 1235 9835 1258", "5632 12** **** 1258"),
+    ("", ""),  # пустая строка
+    ("542", ""),  # короткая
+    ("67890127467815673819837261414", "6789 01** **** 1414")
+])
+def test_get_mask_card_number(input_card, expected_output):
+    assert get_mask_card_number(input_card) == expected_output
+
+
+@pytest.mark.parametrize("input_account, expected_output", [
+    ("84573627496038375638", "**5638"),
+    ("7533 5121 5432", "**5432"),
+    ("", ""),  # пустая строка
+    ("12", ""),  # короткая
+])
+def test_get_mask_account(input_account, expected_output):
+    assert get_mask_account(input_account) == expected_output
