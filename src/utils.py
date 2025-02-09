76f516c116595ff032dc5bd155d067f5f8a14677
Arina <rudenskaaarina@gmail.com>
2025-02-09 17:28:27 +0500

    we are trying boyzz

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,26 @@
 import json
 import os
 
+from config import PATH_HOME
 
-def get_transactions(path: str) -> list:
-    with open(path, 'r') as file:
-        converted_data = json.load(file)
 
-    return list(converted_data)
+def get_transactions(path_name: str) -> list:
+    """
+    Принимает путь до JSON-файла и возвращает список словарей с данными о транзакциях.
+    Если файл пустой, содержит не список или не найден, возвращает пустой список.
+    """
+    try:
+        with open(os.path.join(PATH_HOME, path_name), "r", encoding="utf-8") as file:
+            converted_data: list = json.load(file)
 
-print(get_transactions(r"data\data.json"))
+        if not isinstance(converted_data, list):
+            return []
+
+    except Exception:
+        return []
+
+    return converted_data
+
+
+def get_ruble_transactions(transaction: list) -> list:
+    pass
