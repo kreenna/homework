4ad81df21a6ca96f14f792e9eeaceefa8da52455
Arina <rudenskaaarina@gmail.com>
2025-02-10 18:35:21 +0500

    loggers added, i think. all works

--- src/utils.py
+++ src/utils.py
@@ -1,22 +1,35 @@
 import json
 import os
+import logging
 
 from config import PATH_HOME
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(filename)s - %(levelname)s: %(message)s',
+                    filename='../logs/utils.log',  # запись логов в файл
+                    filemode='w')  # перезапись файла при каждом запуске
 
 def get_transactions(path_name: str) -> list:
     """
     Принимает путь до JSON-файла и возвращает список словарей с данными о транзакциях.
     Если файл пустой, содержит не список или не найден, возвращает пустой список.
     """
+
+    logging.info("Пробуем открыть файл по указанному пути.")
     try:
         with open(os.path.join(PATH_HOME, path_name), "r", encoding="utf-8") as file:
             converted_data: list = json.load(file)
 
+        logging.info("Проверяем корректность формата файла.")
         if not isinstance(converted_data, list):  # проверяем, что файл в нужном формате
             return []
 
-    except Exception:  # любые ошибки с файлом
+    except Exception as error:  # любые ошибки с файлом
+
+        logging.info(f"Произошла ошибка при обработке файла: {error}.")
         return []
 
+    logging.info("Файл прошел проверку.")
     return converted_data
+
+print(get_transactions("data/operations.json"))
